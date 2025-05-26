9169d0a9c1e3448d5ac5600569a187c311c22026
Umar Draz <umardraz1159l@gmail.com>
2025-05-27 00:42:44 +0500

    Add files via upload

---
+++ pre_lab#7_task#4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int factorial_fun(int n) {
+
+int factt=1;
+for(int i=1; i<=n; i++) {
+ factt=factt*i;
+}
+return factt;
+
+}
+void main() {
+    int num;
+    do {
+    printf("Enter a non_negative integer:");
+    scanf("%d",&num);
+    }
+    while(num<0);
+    int fact= factorial_fun(num);
+    printf("%d is the factorial of the %d",fact,num);
+}
